3cbe38c10871a50b56bcc2398b7f79b9be6c55f5
Edouard Deschamps <edouarddeschamps@gmail.com>
2023-05-04 11:16:25 -0400

    premier commit

---
+++ projetPasseport/src/projet_passeport/Passeport.java
@@ -0,0 +1,48 @@
+package projet_passeport;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Passeport {
+
+    public Dose[] doses;
+
+    // passeport sans Dose
+    public Passeport ( )
+    {
+       doses = new Dose[3];
+    }
+    // passeport avec doses déjà données
+    public Passeport ( Dose[] doses)
+    {
+      this.doses = doses;
+    }
+
+    public int possibleDAvoirAutreDose(Dose[] doses)
+    {
+        int i=-1;
+        // vérifier s'il y a une Dose libre sur 3, retourner l'indice ou -1 s'il n'y a plus d'indice libre sur 3
+        for(int j=0;j!=3;j++){
+            String rep=doses[0].getFournisseur();
+            if(rep == null)
+                i++;
+        }
+    return i;
+    }
+
+    public boolean ajouterDose (Dose[] d)
+    {
+        int indice = possibleDAvoirAutreDose(d);
+	    //s'il y a pas de doses possibles, retourner faux
+        if (indice == -1)
+            return false;
+        // si la dose possible est le premier vaccin , rien à vérifier
+        if (indice == 0)
+            return true;
+	// si ce n'est pas le premier vaccin, calculer délai de 6 mois pour savoir si possible, l'ajouter dans le tableau si ce l'est
+        else
+            return true;
+     }
+}
+
+
